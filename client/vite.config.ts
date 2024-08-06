2d4927fa20b9e4952f33a4aa0ed8d6d32827a73a
Diana Shilova <Shilova_de@mail.ru>
2024-08-06 13:22:23 +0300

    fix vite config - delete base

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'frontend-project-12-vite',
   server: {
     proxy: {
       '/api': 'http://localhost:5001',
