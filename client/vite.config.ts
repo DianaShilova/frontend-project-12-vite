79e18f6950e766279dc1451ca4e155625a464b8f
Diana Shilova <Shilova_de@mail.ru>
2024-08-06 12:38:22 +0300

    add base to vite.config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'frontend-project-12-vite',
   server: {
     proxy: {
       '/api': 'http://localhost:5001',
